26cff9eb759a6634f0a41ce6a4e046c8de6b4711
Thomas Geissl <thomas.geissl@gmail.com>
2018-12-12 00:40:15 -0500

    fixed path

--- scripts/linux/install.sh
+++ scripts/linux/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 curl -O https://openframeworks.cc/versions/v0.10.1/of_v0.10.1_linux64gcc6_release.tar.gz
-tar -zxvf of_v0.10.1_osx_release.zip
+tar -zxvf of_v0.10.1_linux64gcc6_release.tar.gz
 rm of_v0.10.1_linux64gcc6_release.tar.gz
 cd of_v0.10.1_linux64gcc6_release/addons
 git clone https://github.com/thomasgeissl/ofxLiveSet.git
