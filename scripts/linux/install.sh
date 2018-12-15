828fbef627d9d647bcab3f2c7f342231d8b75990
Thomas Geissl <thomas.geissl@gmail.com>
2018-12-15 11:33:19 -0500

    added sudo

--- scripts/linux/install.sh
+++ scripts/linux/install.sh
@@ -3,7 +3,7 @@
 curl -O https://openframeworks.cc/versions/v0.10.1/of_v0.10.1_linux64gcc6_release.tar.gz
 tar -zxf of_v0.10.1_linux64gcc6_release.tar.gz
 rm of_v0.10.1_linux64gcc6_release.tar.gz
-of_v0.10.1_linux64gcc6_release/scripts/linux/ubuntu/install_dependencies.sh
+sudo of_v0.10.1_linux64gcc6_release/scripts/linux/ubuntu/install_dependencies.sh -y
 cd of_v0.10.1_linux64gcc6_release/addons
 git clone https://github.com/thomasgeissl/ofxLiveSet.git
 cd ofxLiveSet/scripts
