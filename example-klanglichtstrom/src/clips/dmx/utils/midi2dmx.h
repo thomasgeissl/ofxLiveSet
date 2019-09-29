7ea275c5e9e78d8131892be2af9972f44b11852b
Thomas Geissl <thomas.geissl@gmail.com>
2019-09-29 20:31:02 +0200

    added channel offset

--- example-klanglichtstrom/src/clips/dmx/utils/midi2dmx.h
+++ example-klanglichtstrom/src/clips/dmx/utils/midi2dmx.h
@@ -1,25 +1,34 @@
 #pragma once
 #include "ofxLiveSet.h"
 
-namespace clips {
-    class midi2dmx : public ofxLiveSet::clip::dmx, public ofxLiveSet::clip::midiReactive {
-    public:
-        typedef std::shared_ptr<midi2dmx> pointer;
-        static pointer create()
-        {
-            return std::make_shared<midi2dmx>();
-        }
-        midi2dmx() : ofxLiveSet::clip::dmx(), ofxLiveSet::clip::midiReactive() {
-            _name = "midi2dmx";
-            _active.setName(_name);
-            addParameter(_channel);
-        }
-        
-        void setNoteOn(int note, int velocity) {
-            setValue(note, velocity * 2);
-        }
-        void setNoteOff(int note, int velocity) {
-            setValue(note, 0);
-        }
-    };
+namespace clips
+{
+class midi2dmx : public ofxLiveSet::clip::dmx, public ofxLiveSet::clip::midiReactive
+{
+public:
+    typedef std::shared_ptr<midi2dmx> pointer;
+    static pointer create()
+    {
+        return std::make_shared<midi2dmx>();
+    }
+    midi2dmx() : ofxLiveSet::clip::dmx(), ofxLiveSet::clip::midiReactive()
+    {
+        _name = "midi2dmx";
+        _active.setName(_name);
+        // addParameter(_channel);
+        _channelOffset.set("channelOffset", 23, 0, 255);
+        addParameter(_channelOffset);
+    }
+
+    void setNoteOn(int note, int velocity)
+    {
+        ofLogNotice("midi2dmx") << "note on " << note << ", " << velocity;
+        setValue(note - _channelOffset, velocity * 2);
+    }
+    void setNoteOff(int note, int velocity)
+    {
+        setValue(note - _channelOffset, 0);
+    }
+    ofParameter<int> _channelOffset;
 };
+}; // namespace clips
